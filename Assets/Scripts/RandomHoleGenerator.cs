96f74cdb9a66a5bf581aa60c79ee3accc50ec528
Balint Benedek Toth <toth.balint.benedek@gmail.com>
2021-01-23 13:13:52 +0100

    RandomHoleGenerator.cs improved

--- Assets/Scripts/RandomHoleGenerator.cs
+++ Assets/Scripts/RandomHoleGenerator.cs
@@ -7,11 +7,6 @@ public class RandomHoleGenerator : MonoBehaviour
     [SerializeField] GameObject targetSpot; // the hole and flag
     [SerializeField] GameObject ground;
 
-    // the colliders of the ground
-    BoxCollider2D col1;
-    BoxCollider2D col2;
-    BoxCollider2D col3;
-
     float minX = 1.7f;
     float maxX = 9.6f;
 
@@ -20,21 +15,15 @@ public class RandomHoleGenerator : MonoBehaviour
         float randomX = Random.Range(minX, maxX);
         Instantiate(targetSpot, new Vector3(randomX, 0, 0), Quaternion.identity);
 
+        // the colliders of the ground
         BoxCollider2D[] colliders = ground.GetComponents<BoxCollider2D>();
-
-        col1 = colliders[0];
-        col2 = colliders[1];
-        col3 = colliders[2];
 
         // for randomly moving colliders of the ground
         // to create a hole for ball to roll into
-        float col1ShiftedX = col1.offset.x + randomX + 7.85f;
-        col1.offset = new Vector2(col1ShiftedX, col1.offset.y);
-
-        float col2ShiftedX = col2.offset.x + randomX + 7.85f;
-        col2.offset = new Vector2(col2ShiftedX, col2.offset.y);
-
-        float col3ShiftedX = col3.offset.x + randomX + 7.85f;
-        col3.offset = new Vector2(col3ShiftedX, col3.offset.y);
+        foreach (var collider in colliders)
+        {
+            float colShiftedX = collider.offset.x + randomX + 7.85f;
+            collider.offset = new Vector2(colShiftedX, collider.offset.y);
+        }
     }
 }
